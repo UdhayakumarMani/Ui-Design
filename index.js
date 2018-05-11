cf86c02af49b6abde6d8d10e61e359cb92000940
Udhayakumar Mani <udhayak51993@outlook.com>
2018-05-11 14:01:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ function callback() {
        $(".target").effect( "shake", { times: 1, distance: 3}, 700 );
       } );
     };
+
+//Get class name for parent
+var currentli = $($('.nav').find('a.current')).parent().attr('class');
+var currentl2 = $($('.nav').find('a.current')).parents('.tab1').attr('class');
+alert(currentli);
+alert("currentl2"+currentl2);
+
+//Slide Function for prev and next
+$("#previous").on('click', function(){
+	$("#2DivT").css("left", "-179px").effect("slide", { direction: "left", duration: 900, mode: "hide", queue: false });
+	$("#1DivT").effect("slide", { direction: "right", duration: 900, mode: "show", queue: false });
+});
+
+$("#next").on('click', function(){
+	$("#2DivT").effect("slide", { direction: "left", duration: 900, mode: "show", queue: false }, function() { $(this).css("left", "0"); });
+	$("#1DivT").effect("slide", { direction: "right", duration: 900, mode: "hide", queue: false });
+});
